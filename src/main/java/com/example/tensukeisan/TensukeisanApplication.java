34e3896e110d6aa56497c103c7d76e3ee6c31f32
machida tetsuro <town-field@live.jp>
2023-01-19 00:09:02 +0900

    Initial commit

---
+++ src/main/java/com/example/tensukeisan/TensukeisanApplication.java
@@ -0,0 +1,13 @@
+package com.example.tensukeisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensukeisanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TensukeisanApplication.class, args);
+	}
+
+}
